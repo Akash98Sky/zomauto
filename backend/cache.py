51b10fb445c2373f68922230ed1b6db8fe4943f4
Akash Mondal <a98mondal@gmail.com>
2024-07-18 11:45:52 +0530

    zomauto: update cache manager logic
    * cache on redis if REDIS_URL env var is set otherwise use local caching

--- backend/cache.py
+++ backend/cache.py
@@ -1,21 +1,43 @@
 from os import getenv
+import time
 from typing import Awaitable, Callable, Sequence
 from pydantic import BaseModel
 from redis.asyncio.client import Redis
 import json
+from cacheout import Cache
 
-REDIS_URL = getenv("REDIS_URL", "redis://localhost:6379")
+REDIS_URL = getenv("REDIS_URL")
 
 def init_cache():
     global redis
-    redis = Redis.from_url(REDIS_URL)
+    global local_cache
+    if REDIS_URL:
+        redis = Redis.from_url(REDIS_URL)
+        local_cache = None
+    else:
+        redis = None
+        local_cache = Cache(ttl=604800, timer=time.time, default=None)
 
 class CacheManager:
     def __init__(self, topic: str, namespace: str = "zomauto"):
         self.prefix = f"{namespace}:{topic}"
 
+    async def __get__(self, key: str):
+        if redis:
+            return await redis.get(f"{self.prefix}:{key.lower()}")
+        elif local_cache:
+            return local_cache.get(f"{self.prefix}:{key.lower()}")
+        else:
+            return None
+    
+    async def __set__(self, key: str, value: str, ex: int = 604800):
+        if redis:
+            await redis.set(f"{self.prefix}:{key.lower()}", value, ex=ex)
+        elif local_cache:
+            local_cache.set(f"{self.prefix}:{key.lower()}", value, ttl=ex)
+
     async def cached(self, key: str, func: Callable[[], Awaitable[BaseModel | Sequence[BaseModel]]], expire: int = 604800) -> BaseModel | Sequence[BaseModel]:
-        if (dump := await redis.get(f"{self.prefix}:{key}")) is not None:
+        if (dump := await self.__get__(f"{self.prefix}:{key}")) is not None:
             data = json.loads(dump)
         else:
             data = await func()
@@ -23,7 +45,7 @@ class CacheManager:
                 dump = json.dumps([e.model_dump() for e in data], default=lambda e: e.dict())
             else:
                 dump = json.dumps(data.model_dump(), default=lambda e: e.dict())
-            await redis.set(f"{self.prefix}:{key}", dump, ex=expire)
+            await self.__set__(f"{self.prefix}:{key}", dump, ex=expire)
 
         return data
     
